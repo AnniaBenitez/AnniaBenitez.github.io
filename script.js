f33c543a5128fdba7a36c3e1eeb1b288233ada96
Annia Benítez <anniamicaela@gmail.com>
2023-04-23 15:20:43 -0400

    Se mejoró la interfaz, se terminó de programar el algoritmo :)

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const CALCULAR = document.getElementById('calcular');       //constante direccion del boton calcular
 const ERROR = document.getElementById('error');             //Constante direccion del texto de error
-const FLU = document.getElementById('flu');                 //Constante direccion del texto del flujo
-const MAN = document.getElementById('man');                 //Constane direccion del texto de mantenimiento
+const FLU = document.getElementById('flujo');               //Constante direccion del texto del flujo
+const MAN = document.getElementById('mantenimiento');       //Constane direccion del texto de mantenimiento
+const MANSUP = document.getElementById('mantenimientoSup'); //Constane direccion del texto de mantenimiento
+const MAN1500 = document.getElementById('mant1500');        //Constane direccion del texto de mantenimiento
+const MAN2000 = document.getElementById('mant2000');        //Constane direccion del texto de mantenimiento
+const METODO = document.getElementById('metodo');           //Contiene la direccion de impresion del metodo utilizado
+
 
 /*El listener reacciona ante el boton, al ser presionado
 El evento se encarga de calcular un flujo y mantenimiento según
@@ -9,28 +14,39 @@ el peso ingresado en la caja input*/
 CALCULAR.addEventListener('click', () => {
     const PESO = document.getElementById('peso').value
     if (PESO > 0){
-        ERROR.style.display = 'none'
-        let flujo = calcularFlujo(PESO);
-        let mantenimiento = calcularMantenimientoSuperior(calcularMantenimiento(flujo));
-        FLU.innerHTML = flujo + ' cc/hr';
-        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
-        FLU.style.display = 'block';
-        MAN.style.display = 'block';
+        ERROR.style.display = 'none';
+        calcularFlujo(PESO);
     }
     else{
         ERROR.style.display = 'block';
-        FLU.style.display = 'none';
-        MAN.style.display = 'none';
     }
 });
 
 /*Selecciona el método de calculo de flujo segun el peso */
 function calcularFlujo(peso){
     if(peso <= 30){
-        return calcularHollidaySegar(peso);
+        MAN1500.style.display = 'none';
+        MAN2000.style.display = 'none';
+        METODO.innerHTML = '<b>Metodo utilizado:</b> Holliday-Segar'; 
+        let flujo = calcularHollidaySegar(peso);
+        FLU.innerHTML = '<b>Flujo: </b>' + flujo + ' cc/hr';
+        MAN.innerHTML = '<b>Mantenimiento:</b> ' + calcularMantenimiento(flujo) + ' cc/hr';
+        MANSUP.innerHTML = '<b>Mantenimiento Superior (m+m/2): </b>' + (calcularMantenimiento(flujo)*1.5) + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+        MANSUP.style.display = 'block';
      }
     else{
-        return calcularSuperficieCorporal(peso);
+        MAN.style.display = 'none';
+        MANSUP.style.display = 'none';
+        METODO.innerHTML = '<b>Metodo utilizado:</b> Cálculo de Superficie Corporal'; 
+        let flujo = calcularSuperficieCorporal(peso);
+        FLU.innerHTML = '<b>Flujo: </b>' + flujo + ' cc/hr';
+        MAN1500.innerHTML = '<b>MantenimientoX1500: </b>' + calcularMantenimiento(flujo)*1.5*1500 + ' cc/hr';
+        MAN2000.innerHTML = '<b>MantenimientoX2000: </b>' + calcularMantenimiento(flujo)*1.5*2000 + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN1500.style.display = 'block';
+        MAN2000.style.display = 'block';
     }
 }
 
@@ -49,15 +65,10 @@ function calcularHollidaySegar(peso){
 
 /*Realiza el calculo de flujo según el método de la superficie corporal */
 function calcularSuperficieCorporal(peso){
-    return ( (peso * 4) + 7) / (peso + 90);
+    return (( (peso * 4) + 7) / (peso + 90)).toFixed(3);
 }
 
 /* Calcula el mantenimiento diario según el flujo */
 function calcularMantenimiento(cantidad){
-    return (cantidad/24);
-}
-
-/* Calcula mantenimiento diario superior */
-function calcularMantenimientoSuperior(mantenimiento){
-    return (mantenimiento*1.5);
+    return (cantidad/24).toFixed(3);
 }
